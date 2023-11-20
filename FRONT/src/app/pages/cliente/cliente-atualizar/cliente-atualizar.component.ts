9818eb66b8cd3b76739f70b4f8fd0d99f623373e
Simioni00 <simioni00@outlook.com>
2023-11-20 10:51:10 -0300

    Front Cliente: CRUD - Avisos de Feedback - Funcionando

--- FRONT/src/app/pages/cliente/cliente-atualizar/cliente-atualizar.component.ts
+++ FRONT/src/app/pages/cliente/cliente-atualizar/cliente-atualizar.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Cliente } from '../../../models/cliente.models';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cliente-atualizar',
   templateUrl: './cliente-atualizar.component.html',
-  styleUrl: './cliente-atualizar.component.css'
+  styleUrls: ['./cliente-atualizar.component.css']
 })
-export class ClienteAtualizarComponent {
+export class ClienteAtualizarComponent implements OnInit {
+  cliente : Cliente = { clienteId: 0, nome: '' };
 
+  constructor(private http: HttpClient, private _snackBar: MatSnackBar, private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  atualizarCliente(): void {
+    this.http.put(`https://localhost:7024/api/cliente/atualizar/${this.cliente.clienteId}`, this.cliente)
+      .subscribe({
+        next: (clienteAtualizado) => {
+          console.log('Cliente atualizado:', clienteAtualizado);
+          this._snackBar.open("Cliente atualizado!", "Ok!", {
+            horizontalPosition: "right",
+            verticalPosition: "top",
+          });
+          this.router.navigate(["pages/cliente/buscar"]);
+        },
+        error: (erro) => {
+          console.log(erro);
+          this._snackBar.open("Erro ao atualizar cliente.", "Ok!", {
+            horizontalPosition: "right",
+            verticalPosition: "top",
+          });
+        }
+      });
+  }
 }
+
